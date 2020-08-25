eb37b6edcac9d7927bff7e8330faacb1c6100e0c
devreckas <david.rich27@gmail.com>
2020-08-25 06:39:26 -0600

    mmseqs pipeline fix

--- fb-pruner/src/pipelines/pipeline_mmseqs.c
+++ fb-pruner/src/pipelines/pipeline_mmseqs.c
@@ -167,6 +167,9 @@ void mmseqs_pipeline( WORKER* worker )
       t_cid    = swp;
 
       /* load target and query by looking them up by id (if we aren't using the same from last search) */
+      printf(" t_cid = %d -> %d, q_cid = %d -> %d\n", 
+         t_cid, t_cid_prv, q_cid, q_cid_prv);
+      
       if ( t_cid != t_cid_prv ) {
          printf("# Loading target (%d)...\n", t_cid);
          WORK_load_target_by_id( worker, t_cid );
